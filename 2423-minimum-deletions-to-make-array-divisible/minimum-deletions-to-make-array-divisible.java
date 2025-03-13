df47d6be3180a1494395ec7b3073900ed906fb4a
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-03-13 12:26:42 +0530

    Time: 13 ms (69.38%) | Memory: 64.2 MB (30.82%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
@@ -8,17 +8,13 @@ class Solution {
         return a;
     }
     public int minOperations(int[] nums, int[] numsDivide) {
-        List<Integer> numsList=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            numsList.add(nums[i]);
-        }
         int gcdval=numsDivide[0];
         for(int i=1;i<numsDivide.length;i++){
             gcdval=gcd(gcdval,numsDivide[i]);
         }
-        Collections.sort(numsList);
-        for(int i=0;i<numsList.size();i++){
-            if(gcdval%numsList.get(i)==0){
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(gcdval%nums[i]==0){
                 return i;
             }
         }
