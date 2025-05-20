7219751768b67e2d8a8b6ae0a516089e15e0f621
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-05-20 17:30:49 +0530

    Time: 1 ms (72.87%) | Memory: 45.8 MB (46.81%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,27 +1,35 @@
+import java.util.*;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        ArrayList<Integer> rows=new ArrayList<>();
-        ArrayList<Integer> cols=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    rows.add(i);
-                    cols.add(j);
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        List<int[]> zeroPositions = new ArrayList<>();
+
+        // Step 1: Find all the positions of 0s
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroPositions.add(new int[]{i, j});
                 }
             }
         }
-        for(int row : rows){
-            for(int j=0;j<n;j++){
-                matrix[row][j]=0;
-            }
-        }
-        for(int col : cols){
-            for(int i=0;i<m;i++){
-                matrix[i][col]=0;
-            }
-        }
 
+        // Step 2: Set rows and columns to 0 for each zero position
+        for (int[] pos : zeroPositions) {
+            int row = pos[0];
+            int col = pos[1];
+
+            // Set all elements in the row to 0
+            for (int c = 0; c < m; c++) {
+                matrix[row][c] = 0;
+            }
+
+            // Set all elements in the column to 0
+            for (int r = 0; r < n; r++) {
+                matrix[r][col] = 0;
+            }
+        }
     }
 }
