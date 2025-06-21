70d9ca1730520bcfd9801763eb3e18a86ae4df04
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-06-21 12:22:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,11 +1,10 @@
 class Solution {
     int maxSubarraySum(int[] arr) {
-        // Your code here
         int current=arr[0];
         int max=arr[0];
         for(int i=1;i<arr.length;i++){
             current=Math.max(arr[i],current+arr[i]);
-            max=Math.max(current,max);
+            max=Math.max(max,current);
         }
         return max;
     }
