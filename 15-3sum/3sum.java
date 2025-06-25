eb7de9c028ab2b126e650bcab474df4c75a40519
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-06-25 11:25:57 +0530

    Time: 27 ms (86.23%) | Memory: 51.6 MB (58.81%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,6 +5,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<n-2;i++){
             if(i!=0 && nums[i]==nums[i-1]) continue;
+            if(nums[i]>0) break;
             int j=i+1;
             int k=n-1;
             while(j<k){
