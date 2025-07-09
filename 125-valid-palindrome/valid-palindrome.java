4515e2a9c0c4e45cb3b83d4870ac87faebe8cbf3
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-09 11:15:51 +0530

    Time: 13 ms (42.24%) | Memory: 45 MB (29.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        boolean palindrome=true;
         s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]", "");
-        int n=s.length();
-        int start=0;
-        int end=n-1;
-        while(start<end){
-            if(s.charAt(start)!=s.charAt(end)){
+        String str=s.replaceAll("[^a-z0-9]","");
+        int left=0;
+        int right=str.length()-1;
+        while(left<=right){
+            if(str.charAt(left)!=str.charAt(right)){
                 return false;
             }
-            start++;
-            end--;
+            left++;
+            right--;
         }
         return true;
     }
