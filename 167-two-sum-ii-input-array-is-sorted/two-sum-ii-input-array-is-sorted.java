aafa8792ff09d192ff895767f65588c9befd4c44
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-09 11:10:16 +0530

    Time: 2 ms (93.01%) | Memory: 47.3 MB (51.16%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<numbers.length;i++){
-            int comp=target-numbers[i];
-            if(map.containsKey(comp)){
-                return new int[]{map.get(comp),i+1};
-            }
-            map.put(numbers[i],i+1);
+        int left=0;
+        int right=numbers.length-1;
+        while(left<=right){
+            int sum=numbers[left]+numbers[right];
+            if(sum==target) return new int[]{left+1,right+1};
+            else if(sum>target) right--;
+            else left++;
         }
         return new int[2];
     }
