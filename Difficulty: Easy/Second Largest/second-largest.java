64400ab8b1b0bed9f997a22ff7518b18388c8ade
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-21 06:35:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,48 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-        int maximum=Integer.MIN_VALUE;
+        int max=Integer.MIN_VALUE;
         int n=arr.length;
-        for(int i=0;i<n;i++){
-            if(arr[i]>maximum){
-                maximum=arr[i];
+        for(int num:arr){
+            max=Math.max(max,num);
+        }
+        int secondmax=Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>secondmax && num<max){
+                secondmax=num;
             }
         }
-        int second=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            if(arr[i]>second && arr[i]<maximum){
-                second=arr[i];
-            }
-        }
-        if(second==maximum || second==Integer.MIN_VALUE){
-                second=-1;
-        }
-        return second;
+        return (secondmax==Integer.MIN_VALUE)?-1:secondmax;
     }
 }
