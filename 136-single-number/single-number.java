acc76a48495fbc43758c5d61b4bb152ab1830096
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-21 07:11:44 +0530

    Time: 1 ms (99.88%) | Memory: 46.6 MB (10.35%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int n=nums.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            set.add(nums[i]);
+        int xor=0;
+        for(int num:nums){
+            xor=xor^num;
         }
-        ArrayList<Integer>arr=new ArrayList<>(set);
-        int s=arr.size();
-        int setsum=0;
-        for(int i=0;i<s;i++){
-            setsum+=arr.get(i);
-        }
-        return (setsum*2)-sum;
+    return xor;
     }
 }
