d204d1ea8f809d8cc77cc882ce22243e5b7401d3
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-25 16:59:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.java
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.java
@@ -4,7 +4,6 @@ class Solution {
     public Node insertAtBegining(Node head, int x) {
         // Code here
         Node insert=new Node(x);
-        if(head==null) return insert;
         insert.next=head;
         return insert;
     }
