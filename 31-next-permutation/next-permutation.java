a470e8488f51d9a53cdf1f06cee77b85922abc36
Sumithra Gopal <154010284+Sumithra9@users.noreply.github.com>
2025-07-30 16:03:55 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (74.74%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,45 +1,39 @@
 class Solution {
     public void reverse(int[] nums){
-        int start=0;
-        int end=nums.length-1;
-        while(start<end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
-            start++;
-            end--;
+        int s=0;
+        int e=nums.length-1;
+        int temp;
+        while(s<e){
+            swap(nums,s,e);
+            s++;
+            e--;
         }
     }
-    public void swap(int nums[],int start,int end){
-        int temp=nums[start];
-        nums[start]=nums[end];
-        nums[end]=temp;
+    public void swap(int nums[],int s,int e){
+        int temp=nums[s];
+        nums[s]=nums[e];
+        nums[e]=temp;
     }
     public void nextPermutation(int[] nums) {
-        int b=-1;
-        int n=nums.length;
-        for(int i=n-2;i>=0;i--){
+        int index=-1;
+        for(int i=nums.length-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                b=i;
+                index=i;
                 break;
             }
         }
-        System.out.println(b);
-        if(b==-1){
-            reverse(nums);
-        }
+        if(index==-1) reverse(nums);
         else{
-            int maxafterb=Integer.MAX_VALUE;
-            int index=-1;
-            for(int i=b+1;i<n;i++){
-                if(nums[i]<maxafterb && nums[i]>nums[b]){
-                    maxafterb=nums[i];
-                    index=i;
+            int min=Integer.MAX_VALUE;
+            int sindex=-1;
+            for(int i=index+1;i<nums.length;i++){
+                if(nums[i]<min && nums[i]>nums[index]){
+                    sindex=i;
+                    min=nums[i];
                 }
             }
-            System.out.println(maxafterb+" "+index);
-            swap(nums,b,index);
-            for(int i=b+1;i<nums.length;i++){
+            swap(nums,index,sindex);
+            for(int i=index+1;i<nums.length;i++){
                 for(int j=i+1;j<nums.length;j++){
                     if(nums[i]>nums[j]){
                         swap(nums,i,j);
